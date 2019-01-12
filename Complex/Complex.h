3b157ed8472c4d6574b9b0523dcbd41aeeda6510
FunnyHero <43088722+FunnyHero@users.noreply.github.com>
2019-01-12 04:04:25 +0300

    Add files via upload

---
+++ Complex/Complex.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+	double re, im;
+
+public:
+	Complex() {
+		re = 0;
+		im = 0;
+	}
+	Complex(const double &i_re, const double &i_im) {
+		re = i_re;
+		im = i_im;
+	}
+	Complex(const Complex &copy) {
+		re = copy.re;
+		im = copy.im;
+	}
+	~Complex() {
+
+	}
+
+	friend ostream & operator<< (ostream &, const Complex &);
+	Complex operator + (const Complex &)const;
+	Complex operator * (const Complex &)const;
+	const double abs() const;
+};
+
+Complex Complex::operator + (const Complex &compl)const {
+	return Complex(re + compl.re, im + compl.im);
+}
+
+Complex Complex::operator * (const Complex &c)const {
+	return Complex(re * c.re - im * c.im, re * c.im + im * c.re);
+}
+
+const double Complex::abs() const {
+	return sqrt(re*re + im*im);
+}
+
+ostream& operator<< (ostream &out, const Complex &c) {
+	out << c.re << '+' << '(' << c.im << ')' << 'i';
+	return out;
+}
+
+//void main() {
+//	Complex c1(-1, 2), c2(2, 9), c3;
+//	c3 = c1 + c2;
+//	cout << c3 << endl;
+//	c3 = c1*c2;
+//	cout << c3 << endl;
+//	double a = c3.abs();
+//	cout << a << endl;
+//}
